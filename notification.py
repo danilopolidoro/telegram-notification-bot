291d5f8dfebb435b0c124427c9a7b3423a2f9dec
Danilo Polidoro <danilopolidoro@me.com>
2022-01-02 13:56:30 -0300

    Add gitignore

--- notification.py
+++ notification.py
@@ -6,4 +6,12 @@ class Notification:
 
     def send_message(self, msg:str, chat_id:int):
         self.__BOT.send_message(text=msg, chat_id=chat_id)
-    
+
+    def add_info(self, message:str):
+        '''Add a row of information to the current notification'''
+        pass
+
+    def progress(self, start:int, finish:int, pos:float):
+        '''Add or update a progress bar to the current notification'''
+        pass
+
