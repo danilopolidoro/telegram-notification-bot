bc43494d228def41826164314134b2f278c4081e
Danilo Polidoro <danilopolidoro@me.com>
2022-01-02 14:09:14 -0300

    Lay out plan

--- notification.py
+++ notification.py
@@ -1,17 +1,45 @@
 import telegram
 
 class Notification:
-    def __init__(self, telegram_token:str) -> None:
+    def __init__(self, telegram_token:str, chat_id:int):
         self.__BOT = telegram.Bot(token=telegram_token)
+        self.__CHAT_ID = chat_id
+        self.__msg_stack = []
 
-    def send_message(self, msg:str, chat_id:int):
-        self.__BOT.send_message(text=msg, chat_id=chat_id)
+    def __send_message(self, msg:str):
+        self.__BOT.send_message(text=msg, chat_id=self.__CHAT_ID)
 
-    def add_info(self, message:str):
-        '''Add a row of information to the current notification'''
+    def get_msg_stack(self):
         pass
 
-    def progress(self, start:int, finish:int, pos:float):
-        '''Add or update a progress bar to the current notification'''
+    def edit_msg_stack(self):
         pass
 
+
+class Elements:
+    @staticmethod
+    def completed(msg:str) -> str:
+        return f"✅ {msg}"
+    
+    @staticmethod
+    def green_light(msg:str) -> str:
+        return f"🟢 {msg}"
+
+    @staticmethod
+    def red_light(msg:str) -> str:
+        return f"🔴 {msg}"
+
+class ProgressBar:
+    def __init__(self, start:int, end:int) -> None:
+        self.__START = start
+        self.__END = end
+        self.__pos = start
+
+    def update(self, pos):
+        self.__pos = pos
+
+    def __render(self):
+        pass
+
+    def __str__(self) -> str:
+        return self.__render()
